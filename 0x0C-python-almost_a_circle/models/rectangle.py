bfb14164ac737a3e46db05153f2d5ce2e3b2eb2d
Orjinta Anthony <orjintaxavier@gmail.com>
2022-07-16 20:58:25 +0100

    Update rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -6,6 +6,28 @@ from models.base import Base
 class Rectangle(Base):
     """Represents a rectangle."""
 
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Initialize a new rectangle method.
+        
+        Args:
+            width (int): The width of the new Rectangle.
+            height (int): The height of the new Rectangle.
+            x (int): The x coordinate of the new Rectangle.
+            y (int): The y coordinate of the new Rectangle.
+            id (int): The identity of the new Rectangle.
+            
+        Raises:
+            TypeError: If either of width or height is not an int.
+            ValueError: If either of width or height <= 0.
+            TypeError: If either of x or y is not an int.
+            ValueError: If either of x or y < 0.
+        """
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        super().__init__(id)
+
     @property
     def width(self):
         """width getter function."""
@@ -58,26 +80,6 @@ class Rectangle(Base):
             raise ValueError("y must be >= 0")
         self.__y = value
 
-    def __init__(self, width, height, x=0, y=0, id=None):
-        """Initialize a new rectangle method.
-        Args:
-            width (int): The width of the new Rectangle.
-            height (int): The height of the new Rectangle.
-            x (int): The x coordinate of the new Rectangle.
-            y (int): The y coordinate of the new Rectangle.
-            id (int): The identity of the new Rectangle.
-        Raises:
-            TypeError: If either of width or height is not an int.
-            ValueError: If either of width or height <= 0.
-            TypeError: If either of x or y is not an int.
-            ValueError: If either of x or y < 0.
-        """
-        self.width = width
-        self.height = height
-        self.x = x
-        self.y = y
-        self.__id = super().__init__(id)
-
     def area(self):
         """Defines the area of the rectangle."""
         return self.width * self.height
@@ -94,12 +96,6 @@ class Rectangle(Base):
             [print("#", end="") for i in range(self.width)]
             print("")
 
-    def __str__(self):
-        """Return the print() and str() representation of the Rectangle."""
-        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id,
-                                                       self.x, self.y,
-                                                       self.width, self.height)
-
     def update(self, *args, **kwargs):
         """Assigns an argument to each attribute.
         Args:
@@ -111,36 +107,31 @@ class Rectangle(Base):
                 - 5th argument should be the y attribute.
             **kwargs (dict): New key/value pairs of attributes.
         """
-        if args and len(args) != 0:
-            arg_count = 0
-            for arg in args:
-                if arg_count == 0:
-                    if arg is None:
-                        self.__init__(self.width, self.height, self.x, self.y)
-                    else:
-                        self.id = arg
-                elif arg_count == 1:
-                    self.width == arg
-                elif arg_count == 2:
-                    self.height == arg
-                elif arg_count == 3:
-                    self.x == arg
-                elif arg_count == 4:
-                    self.y == arg
-                arg_count += 1
-
-        elif kwargs and len(kwargs) != 0:
+        attrs = [self.id, self.__width, self.__height, self.__x, self.__y]
+        var = ('id', 'width', 'height', 'x', 'y')
+        if kwargs is not None and (args is None or len(args) == 0):
             for key, value in kwargs.items():
-                if key == "id":
-                    if value is None:
-                        self.__init__(self.width, self.height, self.x, self.y)
-                    else:
-                        self.id = value
-                elif key == "width":
-                    self.width = value
-                elif key == "height":
-                    self.height = value
-                elif key == "x":
-                    self.x = value
-                elif key == "y":
-                    self.y = value
+                if key in var:
+                    attrs[var.index(key)] = value
+            (self.id, self.__width, self.__height, self.__x, self.__y) = attrs
+        else:
+            args_aux = list(attrs[i] for i in range(len(args), 5))
+            args_aux2 = list(args) + args_aux
+            (self.id, self.__width, self.__height,
+             self.__x, self.__y) = args_aux2
+
+    def to_dictionary(self):
+        """Return the dictionary representation of a Rectangle."""
+        return {
+            "id": self.id,
+            "width": self.width,
+            "height": self.height,
+            "x": self.x,
+            "y": self.y
+        }
+
+    def __str__(self):
+        """Return the print() and str() representation of the Rectangle."""
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id,
+                                                       self.x, self.y,
+                                                       self.width, self.height)
