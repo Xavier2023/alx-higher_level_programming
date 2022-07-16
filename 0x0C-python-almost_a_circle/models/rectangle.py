5aac449fe068a9781b1a63e45f4b1f09d8dbe42b
Orjinta Anthony <orjintaxavier@gmail.com>
2022-07-16 21:01:05 +0100

    Update rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -8,14 +8,14 @@ class Rectangle(Base):
 
     def __init__(self, width, height, x=0, y=0, id=None):
         """Initialize a new rectangle method.
-        
+
         Args:
             width (int): The width of the new Rectangle.
             height (int): The height of the new Rectangle.
             x (int): The x coordinate of the new Rectangle.
             y (int): The y coordinate of the new Rectangle.
             id (int): The identity of the new Rectangle.
-            
+
         Raises:
             TypeError: If either of width or height is not an int.
             ValueError: If either of width or height <= 0.
@@ -98,6 +98,7 @@ class Rectangle(Base):
 
     def update(self, *args, **kwargs):
         """Assigns an argument to each attribute.
+
         Args:
             *args (ints): New attribute values.
                 - 1st argument should be the id attribute
